0dfe40b11681e968691e3abd15724d643905b4d6
Kevin Espiñeira <kevin.espineira@northius.com>
2024-06-17 17:10:39 +0200

    feat: Add UptimeRobot types and structs

---
+++ internal/uptimerobot/types.go
@@ -0,0 +1,31 @@
+package uptimerobot
+
+type Monitor struct {
+	ID              int    `json:"id"`
+	FriendlyName    string `json:"friendly_name"`
+	Status          int    `json:"status"`
+	AvgResponseTime string `json:"average_response_time"`
+	Type            int    `json:"type"`
+	URL             string `json:"url"`
+	HttpUsername    string `json:"http_username"`
+	Interval        int    `json:"interval"`
+	KeywordType     string `json:"keyword_type"`
+	KeywordValue    string `json:"keyword_value"`
+	Port            string `json:"port"`
+	SubType         string `json:"sub_type"`
+}
+
+type AccountDetails struct {
+	MonitorLimit   int `json:"monitor_limit"`
+	UpMonitors     int `json:"up_monitors"`
+	DownMonitors   int `json:"down_monitors"`
+	PausedMonitors int `json:"paused_monitors"`
+}
+
+type monitorsResponse struct {
+	Monitors []Monitor `json:"monitors"`
+}
+
+type accountDetailsResponse struct {
+	Account AccountDetails `json:"account"`
+}
