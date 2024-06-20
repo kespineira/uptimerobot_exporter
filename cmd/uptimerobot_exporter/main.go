5115c6472f72c513bdac032e95548064b6c79f1b
Kevin Espiñeira <kevin.espineira@gmail.com>
2024-06-20 14:04:20 +0200

    feat: Add landing page

--- cmd/uptimerobot_exporter/main.go
+++ cmd/uptimerobot_exporter/main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/kespineira/uptimerobot_exporter/collector"
 	"github.com/kespineira/uptimerobot_exporter/config"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/version"
+	"github.com/prometheus/exporter-toolkit/web"
 )
 
 func main() {
@@ -14,5 +16,21 @@ func main() {
 	collector.RegisterCollectors()
 
 	http.Handle("/metrics", promhttp.Handler())
+	landingConfig := web.LandingConfig{
+		Name:        "UpTimeRobot Exporter",
+		Description: "Prometheus exporter for UptimeRobot",
+		Version:     version.Info(),
+		Links: []web.LandingLinks{
+			{
+				Address: "/metrics",
+				Text:    "Metrics",
+			},
+		},
+	}
+	landingPage, err := web.NewLandingPage(landingConfig)
+	if err != nil {
+		log.Fatalf("Error creating landing page: %s", err)
+	}
+	http.Handle("/", landingPage)
 	log.Fatal(http.ListenAndServe(":"+config.Port, nil))
 }
